c0ce1c1deb6233119f6cff68877b0bde731794e1
cong <cong.wang@gl-inet.com>
2023-04-24 18:06:00 +0800

    add

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "9094aacdae789dccd67fa32109ff1a18",
-"index.html": "255c79d4be0ee713955c71af310c03bf",
-"/": "255c79d4be0ee713955c71af310c03bf",
-"main.dart.js": "fe4a0d16d76e18532717def924700597",
+"index.html": "4eefaf38324654fa3fb9c12809d4e42f",
+"/": "4eefaf38324654fa3fb9c12809d4e42f",
+"main.dart.js": "3f48c6e2db432c5812884a759170f233",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
